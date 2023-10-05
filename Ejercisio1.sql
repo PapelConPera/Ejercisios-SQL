a9ee4efc7d7399cd9472e46aac5d69ee7addcb03
PapelConPera <lionel.copajira@gmail.com>
2023-10-05 10:10:08 -0300

    Modifique ejercisios

---
+++ Ejercisio1.sql
@@ -0,0 +1,5 @@
+Consignas
+//1.Mostrar el nombre y duración de las canciones del disco "Outbreak"
+	
+    SELECT name, Milliseconds FROM tracks
+	WHERE AlbumId = (SELECT AlbumId FROM albums WHERE Title = "Outbreak")
---
+++ Ejercisio2.sql
@@ -0,0 +1,5 @@
+Consignas
+//2.Mostrar los títulos de los discos de "Deep Purple"
+
+	SELECT title FROM albums
+	WHERE ArtistId = (SELECT ArtistId FROM artists WHERE name = "Deep Purple")
